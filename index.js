9b425b92c8143a58573185537f73efc2a0f2c227
Alice <alicehermann026@gmail.com>
2024-09-27 21:49:16 -0300

    attt

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const {somar, calculaSalario} = require('./service/exercicios')
+const { somar, calculaSalario } = require('./service/exercicios')
 const app = express();
 app.use(express.json());
 //EXERCICIO 1
 app.post("/ex1", (req, res) => {
 
-    const resultado = somar (req.body.num1, req.body.num2) ;
+    const resultado = somar(req.body.num1, req.body.num2);
     res.json({ message: resultado })
 })
 
 //EXERCICIO 2
 app.post("/ex2", (req, res) => {
-  
+
     const resultado = calculaSalario;
     res.json({ message: resultado })
 })
@@ -53,14 +53,45 @@ app.post("/ex7", (req, res) => {
     res.json({ message: km, m })
 })
 //EX 8
-app.post("/ex8", (req,res) =>{
+app.post("/ex8", (req, res) => {
     const num = req.body.num
-    for (i=0; i<11; i++){
-    let result = num*i}
+    for (i = 0; i < 11; i++) {
+        let result = num * i
+    }
     res.json({ message: result })
 
 
 })
+//ex 9
+app.post("/ex9", (req, res) => {
+    const nota1 = req.body.nota1
+    const nota2 = req.body.nota2
+    const nota3 = req.body.nota3
+    let media = (nota1 + nota2 + nota3) / 3;
+    if (media < 3) {
+        console.log('Reprovado')
+    } else if (media < 7) {
+console.log('Recuperação')
+    } else { 
+        console.log('Aprovado!')
+      }
+      res.json({ message: 'sua média foi: ', media })
+})
+//ex10
+app.post("/ex10", (req, res) => {
+    const genero = req.body.genero
+        const peso = req.body.peso
+    const altura = req.body.altura
+    if (genero == "m"){
+        let imc = (72.7*altura)-58;
+        }else if (genero == "f"){
+        let imc = (62.1*altura)-44.7;
+       }
+      
+console.log("Seu IMC é:", imc)
+})
+
+
 
 //RESPOSTA E CONFIRMAÇÃO
 app.listen(3000, () => {
